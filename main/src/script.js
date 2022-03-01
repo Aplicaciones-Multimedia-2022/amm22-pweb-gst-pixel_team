113e1e4b754bfa9a2004d1e2ef46b39f678f03b2
jortigoso <100358079+jortigoso@users.noreply.github.com>
2022-03-01 11:45:44 +0100

    Update script.js

--- main/src/script.js
+++ main/src/script.js
@@ -1,1 +1,27 @@
 
+console.log("Running");
+
+var nav = document.getElementsByTagName('nav'); 
+const tabs = nav[0].getElementsByTagName('span');
+const sections = document.getElementsByTagName('section');
+
+/*
+const tabs = document.querySelectorAll('.tab');
+const sections = document.querySelectorAll('.content');
+*/
+
+for(var i = 0; i < tabs.length; i++){   
+    tabs[i].addEventListener('click',changeTab);
+}
+
+function changeTab(){
+    for(var i = 0; i < tabs.length; i++){
+        if(tabs[i] == this){
+            tabs[i].classList.add('current');
+            sections[i].classList.add('current');
+        }else{
+            tabs[i].classList.remove('current');
+            sections[i].classList.remove('current');
+        }
+    }
+}
