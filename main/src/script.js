1b3fdeaccbb5be8f5af7fa967a5b730454e4f5b3
jortigoso <100358079+jortigoso@users.noreply.github.com>
2022-03-01 11:56:31 +0100

    done

--- main/src/script.js
+++ main/src/script.js
@@ -1,14 +1,15 @@
 
 console.log("Running");
 
+/*
 var nav = document.getElementsByTagName('nav'); 
 const tabs = nav[0].getElementsByTagName('span');
 const sections = document.getElementsByTagName('section');
+*/
 
-/*
 const tabs = document.querySelectorAll('.tab');
 const sections = document.querySelectorAll('.content');
-*/
+
 
 for(var i = 0; i < tabs.length; i++){   
     tabs[i].addEventListener('click',changeTab);
