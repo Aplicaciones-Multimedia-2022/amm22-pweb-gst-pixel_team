f64785fa63393ed5680985e597f6219aba6ebbd4
jortigoso <100358079+jortigoso@users.noreply.github.com>
2022-03-09 18:51:40 +0100

    Update page.js

--- src/js/page.js
+++ src/js/page.js
@@ -1,1 +1,19 @@
+//Page Logic
+const tabs = document.querySelectorAll('.tab');
+const sections = document.querySelectorAll('.content');
 
+for(var i = 0; i < tabs.length; i++){   
+    tabs[i].addEventListener('click',changeTab);
+}
+
+function changeTab(){
+    for(var i = 0; i < tabs.length; i++){
+        if(tabs[i] == this){
+            tabs[i].classList.add('current');
+            sections[i].classList.add('current');
+        }else{
+            tabs[i].classList.remove('current');
+            sections[i].classList.remove('current');
+        }
+    }
+}
