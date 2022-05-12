5a0b41fc55a84f75aa344281a804d2d28b50874c
jortigoso <100358079+jortigoso@users.noreply.github.com>
2022-05-12 21:24:45 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-gst-launch-1.0 filesrc location=test.mp4 ! qtdemux name=demux \
+gst-launch-1.0 filesrc location=video.mp4 ! qtdemux name=demux \
     oggmux name=mux ! tcpserversink host=127.0.0.1 port=9090 \
     demux.video_0 ! queue ! decodebin ! videoconvert ! videoscale ! 'video/x-raw, width=640, height=480' ! videobalance saturation=0.0 ! theoraenc ! mux. \
     demux.audio_0 ! queue ! decodebin ! audioconvert ! audioresample ! vorbisenc ! mux.
-
-#gst-launch-1.0 filesrc location=test.mp4 ! qtdemux name=demux \
-#    oggmux name=mux ! tcpserversink host=127.0.0.1 port=9090 \
-#    demux.video_0 ! queue ! decodebin ! videoconvert ! queue ! theoraenc ! queue ! mux. !queue !\
-#    demux.audio_0 ! queue ! decodebin ! audioconvert ! audioresample ! vorbisenc ! mux. !queue
